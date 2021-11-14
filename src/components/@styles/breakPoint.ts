21125175527b851a33c6cfbb684ad09152dc9cb4
365kim <365listener@gmail.com>
2021-11-15 00:31:23 +0900

    feat: break-points 추가

---
+++ src/components/@styles/breakPoint.ts
@@ -0,0 +1,7 @@
+export const BREAK_POINT = {
+  MOBILE_SM: '@media (min-width: 280px)',
+  MOBILE_MD: '@media (min-width: 572px)',
+  TABLET_SM: '@media (min-width: 768px)',
+  TABLET_MD: '@media (min-width: 992px)',
+  LAPTOP_SM: '@media (min-width: 1200px)',
+};
--- src/components/@styles/index.ts
+++ src/components/@styles/index.ts
@@ -1,2 +1,3 @@
 export { COLOR } from './color';
 export { GLOBAL_STYLE } from './global';
+export { BREAK_POINT } from './breakPoint';
