228ebe50f243f0841b2049e818389dabc544599d
365kim <365listener@gmail.com>
2021-11-16 10:51:37 +0900

    feat: break-point MOBILE_SM 수정

--- src/components/@styles/breakPoint.ts
+++ src/components/@styles/breakPoint.ts
@@ -1,5 +1,5 @@
 export const BREAK_POINT = {
-  MOBILE_SM: '@media (min-width: 280px)',
+  MOBILE_SM: '@media (min-width: 256px)', // Galaxy Fold 3
   MOBILE_MD: '@media (min-width: 572px)',
   TABLET_SM: '@media (min-width: 768px)',
   TABLET_MD: '@media (min-width: 992px)',
